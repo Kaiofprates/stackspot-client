9dccc1c90d09dd775f68304d5a0c6c167db64444
Kaiofprates <kaiofprates@gmail.com>
2025-05-30 00:01:43 -0300

    feat: :pencil: update readme v.0.1.1

--- stackspot_client/__init__.py
+++ stackspot_client/__init__.py
@@ -1,6 +1,6 @@
 from .client import StackSpotConfig, StackSpotClient, StackSpotError, AuthenticationError, APIError
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     'StackSpotConfig',
